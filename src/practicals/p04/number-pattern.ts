bb53868bb2a7e3cc3492bcc3e4fce3c6d1692137
Bank <nattananpunpetch@gmail.com>
2026-01-21 16:28:46 +0700

    8

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,18 @@
-// Write your code below
+const n = Number(process.argv[2])
+
+if (Number.isNaN(n)||n <= 0 || n % 1 !== 0){
+    process.exit
+}
+
+for (let i = 1; i <= n; i++) {  
+    let str = "".repeat(n-i);  
+    let count = 1;  
+    for (let k = 1; k <= i; k++) {  
+        str = count.toString() + str;  
+        count += 1;  
+    }  
+    for (let h = i; h < n; h++) {  
+        str += " ";  
+    }  
+    console.log(str);  
+} 
