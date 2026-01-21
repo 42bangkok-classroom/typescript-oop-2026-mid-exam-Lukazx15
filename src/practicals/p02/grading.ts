263e519b094789b2680c616f3d4609de38c1e3fb
Bank <nattananpunpetch@gmail.com>
2026-01-21 15:11:19 +0700

    2

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,20 @@
-// Write your code below
+const input = Number(process.argv[2])
+
+if (Number.isNaN(input)||input<0||input>100){
+    console.log('Invalid input')
+}else 
+if (input <= 49){
+    console.log('F')
+}else 
+if (input <= 59){
+    console.log('D')
+}else 
+if (input <= 69){
+    console.log('C')
+}else
+if (input <= 79){
+    console.log('B')
+}else 
+if (input <= 100){
+    console.log('A')
+}
